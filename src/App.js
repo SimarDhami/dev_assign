edc52e5b662878bbba0c67ddd2483ace9191f3a1
SimarDhami <74151780+SimarDhami@users.noreply.github.com>
2023-09-25 12:04:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,31 +7,17 @@ import User from "./pages/UserPage/user";
 import { Navigate } from "react-router-dom";
 import CommonLayout from "./component/commonLayout/commonLayout";
 import SearchResults from "./pages/SeachResults/searchResults";
-import { RequireAuth } from "react-auth-kit";
-import Cart from "./pages/UserPage/SubPages/cart";
-import Borrowed from "./pages/UserPage/SubPages/borrowed";
-import Suggestions from "./pages/UserPage/SubPages/suggestions";
+import { useContext } from "react";
+import { AuthContext } from "./utiils/context/Auth.context.js";
+
 function App() {
-  return (
+   return (
     <Routes>
       <Route path="/" element={<CommonLayout />}>
         <Route index element={<Home />} />
         <Route path="home" element={<Home />} />
-<Route
-          path="user"
-          element={
-            <RequireAuth loginPath="/login">
-              <User />
-            </RequireAuth>
-          }
-        >
-          <Route index element={<Suggestions />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="borrowed" element={<Borrowed />} />
-          <Route path="suggestions" element={<Suggestions />} />
-        </Route>
-
-        <Route path="/search/:searchTerm" element={<SearchResults />} />
+        <Route path="user" element={<User />} />
+<Route path="/search/:searchTerm" element={<SearchResults />} />
 
         <Route path="*" element={<Navigate to="/home" replace />} />
       </Route>
